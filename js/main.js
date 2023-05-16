928cfb63402c0350e7ca94e78caa39ea7d9d3a83
Earl <shepherdmoahloli122@gmail.com>
2023-05-16 21:32:50 +0200

    'Get the moola'

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const of = document.getElementById("currency-form");
+
+of.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const query = document.getElementById("query");
+  const result = document.getElementById("result");
+  const selector = document.getElementById("selector");
+
+  // test if it is a number
+
+  if (isNaN(query.value)) {
+    alert("Please enter a number");
+    of.reset();
+  } else {
+    result.value = query.value * 15;
+  }
+  /*
+  result.value = query.value * 15;
+
+  // Get the selected option
+
+  const selectedOption = selector.options[selector.selectedIndex];
+
+  //result.value = selector.options[selector.selectedIndex].value * query.value;
+  /*
+  if (selector.selectedIndex == 0) {
+    result.value = query.value * 15;
+  } else if (selector.selectedIndex == 1) {
+    result.value = query.value * 25;
+  } else if (selector.selectedIndex == 2) {
+    result.value = query.value * 20;
+  }
+*/
+  console.log(selector.options[selector.selectedIndex]);
+});
